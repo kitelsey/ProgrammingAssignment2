24cb4948a4cd952137c04ef938e6d29749063e31
khietlam1 <khietsune@gmail.com>
2022-02-23 21:39:12 -0500

    first run

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+  inverted.matrx<-NULL
+  set<-function(y){
+   x<<-y
+   inverted.matrx<<-NULL
+ }
+ get<-function() x
+ set.inverted.mtrx<-function(mat) inverted.matrx<<-mat
+ get.inverted.mtrx<-function() inverted.matrx
+   
+ list(set=set,get=get,
+      get.inverted.mtrx=get.inverted.mtrx,
+      set.inverted.mtrx=set.inverted.mtrx)
+ }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ 
+    invrt<-x$get.inverted.mtrx()
+    mtrx.now<-x$get()
+
+    if (is.null(invrt) || !identical(invrt, mtrx.now)){
+      a<-mtrx.now[1]
+      b<--mtrx.now[3]
+      c<--mtrx.now[2]
+      d<-mtrx.now[4]
+      invrt<-x$set.inverted.mtrx(matrix(1/(a*d-b*c)*c(d,c,b,a),nrow=2,ncol=2))
+      return (invrt)
+      }
+      else {
+        print("existed")
+        return (x$get.inverted.mtrx())
+
+      }
 }
